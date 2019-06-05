34d8543b0c0a459375e1c089717889e95213b15d
Ferdi Stoeltie <ferdistoeltie@gmail.com>
2019-06-05 12:45:35 +0200

    added demo projects found on github of wovo

---
+++ projects/blink_test/main.cpp
@@ -0,0 +1,18 @@
+#include "hwlib.hpp"
+
+int main( void ){   
+  
+   namespace target = hwlib::target;
+   auto ds   = target::pin_out( target::pins::d8 );
+   auto shcp = target::pin_out( target::pins::d9 );
+   auto stcp = target::pin_out( target::pins::d10 );
+   auto spi  = hwlib::spi_bus_bit_banged_sclk_mosi_miso( 
+      shcp, 
+      ds, 
+      hwlib::pin_in_dummy 
+   );
+   
+   auto leds = hwlib::hc595( spi, stcp );
+   
+   hwlib::blink( leds.p1 );
+}
